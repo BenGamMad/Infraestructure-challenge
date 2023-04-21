092ed4b9b7b6cebea26e61443dd309cac0a4976d
bgamboam <bgamboam@symbiotic.com>
2023-04-21 17:22:16 -0600

    [BUGFIX] Delete staging and develop from master

---
+++ Server/variables.tf
@@ -0,0 +1,17 @@
+variable "instance_name" {
+  description = "Name tag for the EC2 instance"
+  type        = string
+  default     = "website"
+}
+
+variable "instance_type" {
+  description = "Name of the instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "num_ec2" {
+  description = "Number of identical instances"
+  type = number
+  default = 1
+}
