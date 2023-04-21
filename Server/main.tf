1210bd5c22318f8c4b61ed6ae9016b0eb50f459b
BenGamMad <108495798+BenGamMad@users.noreply.github.com>
2023-04-20 20:22:21 -0600

    Revert "[ADD] Added new Jenkinsfile config"

--- Server/main.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_instance" "Website" {
-  ami = "ami-06e46074ae430fba6"
-  count = var.num_ec2
-  instance_type = var.instance_type
-  tags = {
-    Name = "${var.instance_name}-${count.index}"
-  }
-}
-
